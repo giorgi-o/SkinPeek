f3174aecc4273ed56b883dfc4cbf65706ea719d2
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2022-10-05 22:13:34 +0100

    Show the next battlepass tier reward (Jukki) Bump format version

--- valorant/cache.js
+++ valorant/cache.js
@@ -7,7 +7,7 @@ import {DEFAULT_VALORANT_LANG, discToValLang} from "../misc/languages.js";
 import {client} from "../discord/bot.js";
 import {sendShardMessage} from "../misc/shardMessage.js";
 
-const formatVersion = 7;
+const formatVersion = 8;
 let gameVersion;
 
 let weapons, skins, rarities, buddies, sprays, cards, titles, bundles, battlepass;
